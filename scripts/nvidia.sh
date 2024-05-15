a6029e83d6f0e49e20047bc365705eb3a435b8e7
fei <fei@timesink>
2024-05-15 17:33:27 +0200

    update included public key

--- scripts/nvidia.sh
+++ scripts/nvidia.sh
@@ -2,6 +2,8 @@
 
 set -oue pipefail
 
+install -Dm644 /tmp/certs/private_key.priv /etc/pki/akmods/private/private_key.priv
+
 dnf install -y \
     https://mirrors.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm \
     https://mirrors.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm
