c255989b82d6a5cb5f6894d7a15c5cdc7bd46d9e
fei <fei@timesink>
2024-05-15 14:12:44 +0200

    add copr packages

---
+++ scripts/copr.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+set -oue pipefail
+
+dnf copr enable -y peterwu/rendezvous
+dnf copr enable -y che/nerd-fonts
+dnf copr enable -y kylegospo/system76-scheduler
+dnf config-manager -y --add-repo https://pkgs.tailscale.com/stable/fedora/tailscale.repo
+
+dnf install -y bibata-cursor-themes nerd-fonts system76-scheduler \
+    gnome-shell-extension-system76-scheduler tailscale
+
+systemctl enable com.system76.Scheduler.service
+systemctl enable tailscaled.service
