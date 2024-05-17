60f6e3a869c110e89e907560ed2fe42db4085b9b
fei <fei@timesink>
2024-05-17 14:01:52 +0200

    don't hide nonexisting desktop file

--- scripts/non-repo.sh
+++ scripts/non-repo.sh
@@ -47,7 +47,6 @@ rm -rf /usr/lib/systemd/system/systemd-remount-fs.service
 
 #hide applications
 sed -i 's@\[Desktop Entry\]@\[Desktop Entry\]\nNoDisplay=true@g' /usr/share/applications/org.gnome.Terminal.desktop
-sed -i 's@\[Desktop Entry\]@\[Desktop Entry\]\nNoDisplay=true@g' /usr/share/applications/org.gnome.Tour.desktop
 sed -i 's@\[Desktop Entry\]@\[Desktop Entry\]\nNoDisplay=true@g' /usr/share/applications/nvim.desktop
 
 #fonts
