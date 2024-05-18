bb80d4b3d760268cd69f9e72bef328d5f3aa71f6
fei <fei@timesink>
2024-05-18 21:25:07 +0200

    nvidia debugging

--- scripts/nvidia.sh
+++ scripts/nvidia.sh
@@ -7,12 +7,19 @@ mkdir -p /var/lib/alternatives
 install -Dm644 /tmp/certs/private_key.priv /etc/pki/akmods/private/private_key.priv
 install -Dm644 /usr/etc/pki/akmods/certs/public_key.der /etc/pki/akmods/certs/public_key.der
 
+KVER="$(rpm -q kernel --queryformat '%{VERSION}-%{RELEASE}.%{ARCH}')"
+
 rpm-ostree install mock akmod-nvidia
 rpm-ostree install xorg-x11-drv-nvidia-cuda xorg-x11-drv-nvidia-cuda-libs \
     xorg-x11-drv-nvidia-power nvidia-vaapi-driver libva-utils vdpauinfo
 akmods --force \
-    --kernels "$(rpm -q kernel --queryformat '%{VERSION}-%{RELEASE}.%{ARCH}')" \
+    --kernels "${KVER}"  \
     --kmod nvidia
+
+NVIDIA_AKMOD_VERSION="$(basename "$(rpm -q "akmod-nvidia" --queryformat '%{VERSION}-%{RELEASE}')" ".fc${RELEASE%%.*}")"
+
+modinfo /usr/lib/modules/${KVER}/extra/nvidia/nvidia{,-drm,-modeset,-peermem,-uvm}.ko.xz > /dev/null || \
+(cat /var/cache/akmods/nvidia/${NVIDIA_AKMOD_VERSION}-for-${KVER}.failed.log && exit 1)
 
 systemctl enable nvidia-{suspend,resume,hibernate}
 
