226247786c10603673fdd17dfe014b745bfdaefb
fei <fei@timesink>
2024-05-23 13:50:33 +0200

    let build fail when image desyncs from repos & remove unneeded firmware

--- scripts/nvidia.sh
+++ scripts/nvidia.sh
@@ -12,11 +12,11 @@ dnf install -y \
     https://mirrors.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm
 
 
-dnf install -y akmod-nvidia kernel
+dnf install -y akmod-nvidia
 dnf install -y xorg-x11-drv-nvidia-cuda xorg-x11-drv-nvidia-cuda-libs \
     xorg-x11-drv-nvidia-power nvidia-vaapi-driver libva-utils vdpauinfo
 
-KVER="$(rpm -qa kernel --queryformat '%{VERSION}-%{RELEASE}.%{ARCH}' | tail -n 1)"
+KVER="$(rpm -q kernel --queryformat '%{VERSION}-%{RELEASE}.%{ARCH}')"
 NVIDIA_AKMOD_VERSION="$(rpm -q "akmod-nvidia" --queryformat '%{VERSION}-%{RELEASE}')"
 
 akmods --force \
