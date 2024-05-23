ed641368bddc8f20d6368e6cc45bb555d92dbbd2
fei <fei@timesink>
2024-05-23 20:44:36 +0200

    add default-fonts + minor cleanup

--- scripts/copr.sh
+++ scripts/copr.sh
@@ -7,10 +7,11 @@ dnf install -y dnf-plugins-core
 dnf copr enable -y peterwu/rendezvous
 dnf copr enable -y che/nerd-fonts
 dnf copr enable -y kylegospo/system76-scheduler
+dnf copr enable -y wezfurlong/wezterm-nightly
 dnf config-manager -y --add-repo https://pkgs.tailscale.com/stable/fedora/tailscale.repo
 
 dnf install -y bibata-cursor-themes nerd-fonts system76-scheduler \
-    gnome-shell-extension-system76-scheduler tailscale
+    gnome-shell-extension-system76-scheduler tailscale wezterm
 
 systemctl enable com.system76.Scheduler.service
 systemctl enable tailscaled.service
@@ -18,4 +19,5 @@ systemctl enable tailscaled.service
 dnf copr disable -y peterwu/rendezvous
 dnf copr disable -y che/nerd-fonts
 dnf copr disable -y kylegospo/system76-scheduler
+dnf copr disable -y wezfurlong/wezterm-nightly
 dnf config-manager -y --disable tailscale-stable
