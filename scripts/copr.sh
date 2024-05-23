5e76cff0eedf1022950a31a4c2254283ccb0638d
fei <fei@timesink>
2024-05-24 00:46:13 +0200

    revert install wezterm from copr

--- scripts/copr.sh
+++ scripts/copr.sh
@@ -7,11 +7,10 @@ dnf install -y dnf-plugins-core
 dnf copr enable -y peterwu/rendezvous
 dnf copr enable -y che/nerd-fonts
 dnf copr enable -y kylegospo/system76-scheduler
-dnf copr enable -y wezfurlong/wezterm-nightly
 dnf config-manager -y --add-repo https://pkgs.tailscale.com/stable/fedora/tailscale.repo
 
 dnf install -y bibata-cursor-themes nerd-fonts system76-scheduler \
-    gnome-shell-extension-system76-scheduler tailscale wezterm
+    gnome-shell-extension-system76-scheduler tailscale
 
 systemctl enable com.system76.Scheduler.service
 systemctl enable tailscaled.service
@@ -19,5 +18,4 @@ systemctl enable tailscaled.service
 dnf copr disable -y peterwu/rendezvous
 dnf copr disable -y che/nerd-fonts
 dnf copr disable -y kylegospo/system76-scheduler
-dnf copr disable -y wezfurlong/wezterm-nightly
 dnf config-manager -y --disable tailscale-stable
