6e32d43dd0a657fc0d9f40874939139d7cdedd90
fei <fei@timesink>
2024-06-04 13:42:51 +0200

    Update dconf defaults

--- scripts/copr.sh
+++ scripts/copr.sh
@@ -4,18 +4,16 @@ set -oue pipefail
 
 dnf install -y dnf-plugins-core
 
-dnf copr enable -y peterwu/rendezvous
 dnf copr enable -y che/nerd-fonts
 dnf copr enable -y kylegospo/system76-scheduler
 dnf config-manager -y --add-repo https://pkgs.tailscale.com/stable/fedora/tailscale.repo
 
-dnf install -y bibata-cursor-themes nerd-fonts system76-scheduler \
+dnf install -y nerd-fonts system76-scheduler \
     gnome-shell-extension-system76-scheduler tailscale
 
 systemctl enable com.system76.Scheduler.service
 systemctl enable tailscaled.service
 
-dnf copr disable -y peterwu/rendezvous
 dnf copr disable -y che/nerd-fonts
 dnf copr disable -y kylegospo/system76-scheduler
 dnf config-manager -y --disable tailscale-stable
