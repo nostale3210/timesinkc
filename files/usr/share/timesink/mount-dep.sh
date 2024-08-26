3bfe78e2d4802534b78a4e792d04807ee83a622f
feiluuyre <fei@timesink>
2024-08-26 15:34:33 +0200

    add situational mounts for nx update tsd

--- files/usr/share/timesink/mount-dep.sh
+++ files/usr/share/timesink/mount-dep.sh
@@ -13,18 +13,21 @@ mount -o rbind /run run/
 mount -o bind,rw /var var/
 mount -o bind,rw /home home/
 mount -o bind,rw /mnt mnt/
-mount -o bind,rw /etc etc/
 mount -o bind,rw /deploy deploy/
 
 mkdir -p deploy/og
-mount -o bind / deploy/og
+mount -o bind,ro / deploy/og
 
 mount -o bind,ro usr usr
+mount -o bind,rw usr/local usr/local
 chattr +i .
 
 echo "INSERT_DEPLOYMENT" > deploy/current
 
-mkdir -p var/.etc{,work}
-mount -t overlay overlay -o lowerdir=etc,upperdir=var/.etc,workdir=var/.etcwork etc
+mkdir -p deploy/.var{,work}
+mount -t overlay var-overlay -o lowerdir=var:deploy/var.INSERT_DEPLOYMENT,upperdir=deploy/.var,workdir=deploy/.varwork var
+
+mkdir -p deploy/.etc{,work}
+mount -t overlay etc-overlay -o lowerdir=etc,upperdir=deploy/.etc,workdir=deploy/.etcwork etc
 
 exec /usr/sbin/switch_root /deploy/INSERT_DEPLOYMENT "/usr/sbin/init" "$@"
