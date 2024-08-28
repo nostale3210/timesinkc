4e98d949c27840d5a03a5d7f9aca0cb71634059a
feiluuyre <fei@timesink>
2024-08-28 12:17:04 +0200

    include statically linked binaries for init

--- files/usr/share/timesink/atomic/init.sh
+++ files/usr/share/timesink/atomic/init.sh
@@ -1,10 +1,11 @@
-#!/usr/bin/bash
+#!/var/deployments/init/safe/busybox ash
 
-if [[ "$(cat /var/deployments/current)" != "INSERT_DEPLOYMENT" ]]; then
-    exch /usr /var/deployments/INSERT_DEPLOYMENT/usr
-    exch /etc /var/deployments/INSERT_DEPLOYMENT/etc
-    mv /var/deployments/INSERT_DEPLOYMENT /var/deployments/"$(cat /var/deployments/current)"
-    echo "INSERT_DEPLOYMENT" > /var/deployments/current
+if [[ "$(/var/deployments/init/safe/busybox cat /var/deployments/current)" != "INSERT_DEPLOYMENT" ]]; then
+    /var/deployments/init/safe/busybox mount -o remount,rw /
+    /var/deployments/init/safe/jexch -e /usr /var/deployments/INSERT_DEPLOYMENT/usr || break
+    /var/deployments/init/safe/jexch -e /etc /var/deployments/INSERT_DEPLOYMENT/etc
+    /var/deployments/init/safe/busybox mv /var/deployments/INSERT_DEPLOYMENT /var/deployments/"$(/var/deployments/init/safe/busybox cat /var/deployments/current)"
+    /var/deployments/init/safe/busybox echo "INSERT_DEPLOYMENT" > /var/deployments/current
 fi
 
 mount -o bind,ro /usr /usr
