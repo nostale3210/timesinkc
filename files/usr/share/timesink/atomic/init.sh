8c519d1c64d43feceaf3d365eb11503e5647ec92
feiluuyre <fei@timesink>
2024-08-29 13:19:05 +0200

    pod-up: ignore containers with localhost
    ald: add update check
    
    ald: help & status, cleanup

--- files/usr/share/timesink/atomic/init.sh
+++ files/usr/share/timesink/atomic/init.sh
@@ -1,11 +1,12 @@
-#!ALD_PATH/init/safe/busybox ash
+#!ALD_PATH/init/safe/busybox sh
 
-if [[ "$(ALD_PATH/init/safe/busybox cat ALD_PATH/current)" != "INSERT_DEPLOYMENT" ]]; then
+if [ "$(ALD_PATH/init/safe/busybox cat ALD_PATH/current)" != "INSERT_DEPLOYMENT" ]; then
     ALD_PATH/init/safe/busybox mount -o remount,rw /
-    ALD_PATH/init/safe/jexch -e /usr ALD_PATH/INSERT_DEPLOYMENT/usr || break
-    ALD_PATH/init/safe/jexch -e /etc ALD_PATH/INSERT_DEPLOYMENT/etc
-    ALD_PATH/init/safe/busybox mv ALD_PATH/INSERT_DEPLOYMENT ALD_PATH/"$(ALD_PATH/init/safe/busybox cat ALD_PATH/current)"
-    ALD_PATH/init/safe/busybox echo "INSERT_DEPLOYMENT" > ALD_PATH/current
+    if ALD_PATH/init/safe/jexch -e /usr ALD_PATH/INSERT_DEPLOYMENT/usr; then
+        ALD_PATH/init/safe/jexch -e /etc ALD_PATH/INSERT_DEPLOYMENT/etc
+        ALD_PATH/init/safe/busybox mv ALD_PATH/INSERT_DEPLOYMENT ALD_PATH/"$(ALD_PATH/init/safe/busybox cat ALD_PATH/current)"
+        ALD_PATH/init/safe/busybox echo "INSERT_DEPLOYMENT" > ALD_PATH/current
+    fi
 fi
 
 mount -o bind,ro /usr /usr
