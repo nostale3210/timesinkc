0da4eb3d77bfefd3a16544270d15b326479937b8
feiluuyre <fei@timesink>
2024-11-21 14:30:47 +0100

    strip unnecessary packages from cosmic-desktop-environment
    Bump hhd-dev/rechunk from 0.8.6 to 1.0.1
    
    Bumps [hhd-dev/rechunk](https://github.com/hhd-dev/rechunk) from 0.8.6 to 1.0.1.
    - [Release notes](https://github.com/hhd-dev/rechunk/releases)
    - [Changelog](https://github.com/hhd-dev/rechunk/blob/master/example_changelog.md)
    - [Commits](https://github.com/hhd-dev/rechunk/compare/v0.8.6...v1.0.1)
    
    ---
    updated-dependencies:
    - dependency-name: hhd-dev/rechunk
      dependency-type: direct:production
      update-type: version-update:semver-major
    ...
    
    Signed-off-by: dependabot[bot] <support@github.com>
    
    Bump actions/upload-artifact from 4.4.0 to 4.4.3
    
    Bumps [actions/upload-artifact](https://github.com/actions/upload-artifact) from 4.4.0 to 4.4.3.
    - [Release notes](https://github.com/actions/upload-artifact/releases)
    - [Commits](https://github.com/actions/upload-artifact/compare/v4.4.0...v4.4.3)
    
    ---
    updated-dependencies:
    - dependency-name: actions/upload-artifact
      dependency-type: direct:production
      update-type: version-update:semver-patch
    ...
    
    Signed-off-by: dependabot[bot] <support@github.com>
    
    strip unnecessary packages from cosmic-desktop-environment

--- scripts/cosmic.sh
+++ scripts/cosmic.sh
@@ -3,7 +3,8 @@ set -oue pipefail
 
 dnf5 config-manager -y setopt rpmfusion-free.enabled=1 rpmfusion-free-updates.enabled=1 \
     rpmfusion-nonfree.enabled=1 rpmfusion-nonfree-updates.enabled=1
-dnf5 install -y @cosmic-desktop-environment
+dnf5 install -y @cosmic-desktop-environment --skip-broken \
+    --exclude=thunderbird,gnome-calculator,nheko,okular,rhythmbox,abrt,setroubleshoot
 dnf5 install -y pavucontrol gnome-tweaks steam vulkan-tools seahorse
 dnf5 config-manager -y setopt rpmfusion-free.enabled=0 rpmfusion-free-updates.enabled=0 \
     rpmfusion-nonfree.enabled=0 rpmfusion-nonfree-updates.enabled=0
