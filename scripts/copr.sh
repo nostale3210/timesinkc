f94a2e821a7ecea9719fea9d02e114c5b753dc5f
feiluuyre <fei@timesink>
2024-12-17 17:24:24 +0100

    use official fedora container org for all images

--- scripts/copr.sh
+++ scripts/copr.sh
@@ -1,18 +1,18 @@
 #!/usr/bin/env bash
 set -oue pipefail
 
-dnf5 copr enable -y che/nerd-fonts
-dnf5 copr enable -y kylegospo/system76-scheduler
-dnf5 config-manager -y addrepo --from-repofile=https://pkgs.tailscale.com/stable/fedora/tailscale.repo
+dnf copr enable -y che/nerd-fonts
+dnf copr enable -y kylegospo/system76-scheduler
+dnf config-manager -y addrepo --from-repofile=https://pkgs.tailscale.com/stable/fedora/tailscale.repo
 
-dnf5 install -y nerd-fonts system76-scheduler \
+dnf install -y nerd-fonts system76-scheduler \
     tailscale
 
 systemctl enable com.system76.Scheduler.service
 systemctl enable tailscaled.service
 
-dnf5 copr disable -y che/nerd-fonts
-dnf5 copr disable -y kylegospo/system76-scheduler
-dnf5 config-manager -y setopt tailscale-stable.enabled=0
+dnf copr disable -y che/nerd-fonts
+dnf copr disable -y kylegospo/system76-scheduler
+dnf config-manager -y setopt tailscale-stable.enabled=0
 
-dnf5 remove -y toolbox
+dnf remove -y toolbox
--- scripts/drivers.sh
+++ scripts/drivers.sh
@@ -2,26 +2,26 @@
 
 set -oue pipefail
 
-dnf5 install -y \
+dnf install -y \
     https://mirrors.rpmfusion.org/free/fedora/rpmfusion-free-release-"$(rpm -E %fedora)".noarch.rpm \
     https://mirrors.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-"$(rpm -E %fedora)".noarch.rpm
 
-dnf5 install -y --best mesa-dri-drivers
+dnf install -y --best mesa-dri-drivers
 MESAVER="$(rpm -q mesa-dri-drivers --queryformat '%{VERSION}-%{RELEASE}')"
-dnf5 install -y mesa-filesystem-"${MESAVER}" \
+dnf install -y mesa-filesystem-"${MESAVER}" \
     mesa-libEGL-"${MESAVER}" mesa-libGL-"${MESAVER}" \
     mesa-libgbm-"${MESAVER}" mesa-libglapi-"${MESAVER}" \
     mesa-vulkan-drivers-"${MESAVER}"
 
-dnf5 install -y intel-media-driver libva-intel-driver
-dnf5 config-manager -y setopt updates-testing.enabled=1 &&
-    dnf5 config-manager -y setopt rpmfusion-free-updates-testing.enabled=1 &&
-    dnf5 swap -y mesa-va-drivers mesa-va-drivers-freeworld-"${MESAVER}" &&
-    dnf5 swap -y mesa-vdpau-drivers mesa-vdpau-drivers-freeworld-"${MESAVER}" &&
-    dnf5 config-manager -y setopt updates-testing.enabled=0 &&
-    dnf5 config-manager -y setopt rpmfusion-free-updates-testing.enabled=0
-dnf5 swap -y ffmpeg-free ffmpeg --allowerasing
-dnf5 install -y steam-devices gamescope
+dnf install -y intel-media-driver libva-intel-driver
+dnf config-manager -y setopt updates-testing.enabled=1 &&
+    dnf config-manager -y setopt rpmfusion-free-updates-testing.enabled=1 &&
+    dnf swap -y mesa-va-drivers mesa-va-drivers-freeworld-"${MESAVER}" &&
+    dnf swap -y mesa-vdpau-drivers mesa-vdpau-drivers-freeworld-"${MESAVER}" &&
+    dnf config-manager -y setopt updates-testing.enabled=0 &&
+    dnf config-manager -y setopt rpmfusion-free-updates-testing.enabled=0
+dnf swap -y ffmpeg-free ffmpeg --allowerasing
+dnf install -y steam-devices gamescope
 
-dnf5 config-manager -y setopt rpmfusion-free.enabled=0 rpmfusion-free-updates.enabled=0 \
+dnf config-manager -y setopt rpmfusion-free.enabled=0 rpmfusion-free-updates.enabled=0 \
     rpmfusion-nonfree.enabled=0 rpmfusion-nonfree-updates.enabled=0
