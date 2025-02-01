c62ab8fdd50fbc306c3ef429db00ef46c655cd34
feiluuyre <fei@timesink>
2025-02-01 13:13:42 +0100

    use fedora-bootc image for gnome

--- scripts/gnome.sh
+++ scripts/gnome.sh
@@ -6,13 +6,19 @@ dnf install -y gnome-shell-extension-system76-scheduler kitty nautilus-python\
     papirus-icon-theme gnome-tweaks
 dnf copr disable -y kylegospo/system76-scheduler
 
+dnf install -y @workstation-product-environment --skip-broken \
+    --exclude=thunderbird,gnome-calculator,nheko,okular,rhythmbox,abrt,setroubleshoot,libreoffice-core \
+    --exclude=evince,yelp,gnome-boxes,gnome-calendar,gnome-characters,gnome-clocks,gnome-connections \
+    --exclude=gnome-contacts,gnome-font-viewer,gnome-maps,gnome-weather,gnome-logs,gnome-remote-desktop \
+    --exclude=PackageKit,sudshi,totem,snapshot,gnome-text-editor,loupe,mediawriter,simple-scan,toolbox \
+    --exclude=baobab
+dnf install -y fedora-release-silverblue fuse-libs evince-previewer evince-thumbnailer tuned tuned-ppd
+
 dnf remove -y gnome-terminal-nautilus
 
 pip install --prefix=/usr nautilus-open-any-terminal
 
 # sed -i 's@\[Desktop Entry\]@\[Desktop Entry\]\nNoDisplay=true@g' /usr/share/applications/org.gnome.Terminal.desktop
-sed -i 's@\[Desktop Entry\]@\[Desktop Entry\]\nNoDisplay=true@g' /usr/share/applications/org.gnome.Tour.desktop
-sed -i 's@\[Desktop Entry\]@\[Desktop Entry\]\nNoDisplay=true@g' /usr/share/applications/yelp.desktop
 
 wget -cO- https://github.com/phisch/phinger-cursors/releases/latest/download/phinger-cursors-variants.tar.bz2 |
     tar xfj - -C /usr/share/icons
