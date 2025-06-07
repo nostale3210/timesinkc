3d080cbd19c8a3fe8d43e09fefcc8b705fa6c0b1
fei <fei@timesink>
2025-06-07 22:04:29 +0200

    install nvidia from negativo17
    build with stock kernel

--- Containerfile
+++ Containerfile
@@ -44,13 +44,13 @@ RUN mandb && bash librarizer && \
 RUN bash dep_check && \
     rm -f dep_check
 
-RUN sed -i "s/tsd upgrade/hald dep -uzsag/g" /usr/libexec/sys-up && \
+RUN sed -i "s/tsd upgrade/hald dep -uzag/g" /usr/libexec/sys-up && \
     sed -i "s/\"bootc\"/\"hald\"/g" /usr/libexec/sys-up
 
 RUN chmod 4755 /usr/bin/newgidmap && \
     chmod 4755 /usr/bin/newuidmap
 
-RUN KVER="$(rpm -q kernel-cachyos --queryformat '%{VERSION}-%{RELEASE}.%{ARCH}')" && \
+RUN KVER="$(rpm -q kernel-core --queryformat '%{VERSION}-%{RELEASE}.%{ARCH}')" && \
     dracut --no-hostonly --kver "$KVER" --reproducible -v \
     --add "ald tpm2-tss systemd-pcrphase plymouth" -f "/lib/modules/$KVER/initramfs.img" && \
     chmod 0600 "/lib/modules/$KVER/initramfs.img"
--- scripts/nvidia.sh
+++ scripts/nvidia.sh
@@ -6,35 +6,27 @@ mkdir -p /var/lib/alternatives
 
 install -Dm644 /tmp/certs/private_key.priv /etc/pki/akmods/private/private_key.priv
 
-dnf config-manager -y setopt rpmfusion-free.enabled=1 rpmfusion-free-updates.enabled=1 \
-    rpmfusion-nonfree.enabled=1 rpmfusion-nonfree-updates.enabled=1
+dnf config-manager -y addrepo --from-repofile=https://negativo17.org/repos/fedora-nvidia.repo
 
-dnf install -y akmod-nvidia
-dnf install -y --allowerasing xorg-x11-drv-nvidia-cuda xorg-x11-drv-nvidia-cuda-libs \
-    xorg-x11-drv-nvidia-power nvidia-vaapi-driver libva-utils vdpauinfo
+dnf install -y nvidia-driver akmod-nvidia libva-nvidia-driver
 
 # KVER="$(rpm -q kernel-core --queryformat '%{VERSION}-%{RELEASE}')"
-KVER_LONG="$(rpm -q kernel-cachyos --queryformat '%{VERSION}-%{RELEASE}.%{ARCH}')"
+KVER_LONG="$(rpm -q kernel-core --queryformat '%{VERSION}-%{RELEASE}.%{ARCH}')"
 NVIDIA_AKMOD_VERSION="$(rpm -q "akmod-nvidia" --queryformat '%{VERSION}-%{RELEASE}')"
 
-rm -f /etc/nvidia/kernel.conf &&
-    mkdir -p /etc/nvidia &&
-    echo "MODULE_VARIANT=kernel" > /etc/nvidia/kernel.conf
+sed -i -e 's/kernel-open$/kernel/g' /etc/nvidia/kernel.conf
 
 akmods --force \
     --kernels "${KVER_LONG}" \
     --kmod "nvidia"
 
-modinfo /usr/lib/modules/"${KVER_LONG}"/extra/nvidia/nvidia{,-drm,-modeset,-peermem,-uvm}.ko > /dev/null || \
-(cat /var/cache/akmods/nvidia/"${NVIDIA_AKMOD_VERSION::-5}"-for-"${KVER_LONG}".failed.log && exit 1)
+# modinfo /usr/lib/modules/"${KVER_LONG}"/extra/nvidia/nvidia{,-drm,-modeset,-peermem,-uvm}.ko > /dev/null || \
+# (cat /var/cache/akmods/nvidia/"${NVIDIA_AKMOD_VERSION::-5}"-for-"${KVER_LONG}".failed.log && exit 1)
 
-modinfo /usr/lib/modules/"${KVER_LONG}"/extra/nvidia/nvidia.ko
-
-systemctl enable nvidia-{suspend,resume,hibernate}
+modinfo /usr/lib/modules/"${KVER_LONG}"/extra/nvidia/nvidia.ko.xz
 
 rm -rf /etc/pki/akmods/private/private_key.priv
 
-dnf config-manager -y setopt rpmfusion-free.enabled=0 rpmfusion-free-updates.enabled=0 \
-    rpmfusion-nonfree.enabled=0 rpmfusion-nonfree-updates.enabled=0
+dnf config-manager -y setopt fedora-nvidia.enabled=0
 
 dnf autoremove -y
