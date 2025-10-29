91614181b0072d10ad7619f0f1eacf61dc923e9c
fei <fei@timesink>
2025-10-30 00:16:29 +0100

    build with cachyos lto kernel

--- scripts/copr.sh
+++ scripts/copr.sh
@@ -2,7 +2,7 @@
 set -oue pipefail
 
 dnf copr enable -y che/nerd-fonts
-dnf copr enable -y bieszczaders/kernel-cachyos
+dnf copr enable -y bieszczaders/kernel-cachyos-lto
 dnf copr enable -y bieszczaders/kernel-cachyos-addons
 dnf copr enable -y chenxiaolong/sbctl
 dnf config-manager -y addrepo --from-repofile=https://pkgs.tailscale.com/stable/fedora/tailscale.repo
@@ -25,10 +25,10 @@ dnf install -y nerd-fonts tailscale librewolf sbctl
 
 dnf install -y libcap-ng libcap-ng-devel procps-ng procps-ng-devel
 
-dnf install -y kernel-cachyos kernel-cachyos-devel-matched scx-scheds cachyos-ksm-settings
+dnf install -y kernel-cachyos-lto kernel-cachyos-lto-devel-matched scx-scheds
 
 dnf copr disable -y che/nerd-fonts
-dnf copr disable -y bieszczaders/kernel-cachyos
+dnf copr disable -y bieszczaders/kernel-cachyos-lto
 dnf copr disable -y bieszczaders/kernel-cachyos-addons
 dnf copr disable -y chenxiaolong/sbctl
 dnf config-manager -y setopt tailscale-stable.enabled=0 librewolf.enabled=0
--- scripts/nvidia.sh
+++ scripts/nvidia.sh
@@ -1,20 +1,20 @@
 #!/usr/bin/env bash
 
-set -oue pipefail
+set -ouex pipefail
 
 mkdir -p /var/lib/alternatives
 
 install -Dm644 /tmp/certs/private_key.priv /etc/pki/akmods/private/private_key.priv
 
-# dnf copr enable -y bieszczaders/kernel-cachyos-lto
+dnf copr enable -y bieszczaders/kernel-cachyos-lto
 dnf config-manager -y addrepo --from-repofile=https://negativo17.org/repos/fedora-nvidia.repo
 
-# dnf install -y akmods --from-repo copr:copr.fedorainfracloud.org:bieszczaders:kernel-cachyos-lto
+dnf install -y akmods --from-repo copr:copr.fedorainfracloud.org:bieszczaders:kernel-cachyos-lto
 
 dnf install -y nvidia-driver akmod-nvidia libva-nvidia-driver nvidia-driver-cuda
 
 # KVER="$(rpm -q kernel-core --queryformat '%{VERSION}-%{RELEASE}')"
-KVER_LONG="$(rpm -q kernel-cachyos --queryformat '%{VERSION}-%{RELEASE}.%{ARCH}')"
+KVER_LONG="$(rpm -q kernel-cachyos-lto --queryformat '%{VERSION}-%{RELEASE}.%{ARCH}')"
 NVIDIA_AKMOD_VERSION="$(rpm -q "akmod-nvidia" --queryformat '%{VERSION}-%{RELEASE}')"
 
 sed -i -e 's/kernel-open$/kernel/g' /etc/nvidia/kernel.conf
@@ -30,7 +30,7 @@ modinfo /usr/lib/modules/"${KVER_LONG}"/extra/nvidia/nvidia.ko.xz
 
 rm -rf /etc/pki/akmods/private/private_key.priv
 
-# dnf copr disable -y bieszczaders/kernel-cachyos-lto
+dnf copr disable -y bieszczaders/kernel-cachyos-lto
 dnf config-manager -y setopt fedora-nvidia.enabled=0
 
 dnf autoremove -y
