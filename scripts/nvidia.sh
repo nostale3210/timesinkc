ec823d39ad24fd5649e743d6c27837e83b5c81ff
fei <fei@timesink>
2025-12-06 17:17:35 +0100

    use lts nvidia 580 repo

--- scripts/nvidia.sh
+++ scripts/nvidia.sh
@@ -7,7 +7,7 @@ mkdir -p /var/lib/alternatives
 install -Dm644 /tmp/certs/private_key.priv /etc/pki/akmods/private/private_key.priv
 
 dnf copr enable -y bieszczaders/kernel-cachyos-lto
-dnf config-manager -y addrepo --from-repofile=https://negativo17.org/repos/fedora-nvidia.repo
+dnf config-manager -y addrepo --from-repofile=https://negativo17.org/repos/fedora-nvidia-580.repo
 
 dnf install -y akmods --from-repo copr:copr.fedorainfracloud.org:bieszczaders:kernel-cachyos-lto
 
@@ -17,8 +17,6 @@ dnf install -y nvidia-driver akmod-nvidia libva-nvidia-driver nvidia-driver-cuda
 KVER_LONG="$(rpm -q kernel-cachyos-lto --queryformat '%{VERSION}-%{RELEASE}.%{ARCH}')"
 NVIDIA_AKMOD_VERSION="$(rpm -q "akmod-nvidia" --queryformat '%{VERSION}-%{RELEASE}')"
 
-sed -i -e 's/kernel-open$/kernel/g' /etc/nvidia/kernel.conf
-
 akmods --force \
     --kernels "${KVER_LONG}" \
     --kmod "nvidia"
@@ -26,11 +24,11 @@ akmods --force \
 # modinfo /usr/lib/modules/"${KVER_LONG}"/extra/nvidia/nvidia{,-drm,-modeset,-peermem,-uvm}.ko > /dev/null || \
 # (cat /var/cache/akmods/nvidia/"${NVIDIA_AKMOD_VERSION::-5}"-for-"${KVER_LONG}".failed.log && exit 1)
 
-modinfo /usr/lib/modules/"${KVER_LONG}"/extra/nvidia/nvidia.ko.xz
+modinfo -l /usr/lib/modules/"${KVER_LONG}"/extra/nvidia/nvidia.ko.xz
 
 rm -rf /etc/pki/akmods/private/private_key.priv
 
 dnf copr disable -y bieszczaders/kernel-cachyos-lto
-dnf config-manager -y setopt fedora-nvidia.enabled=0
+dnf config-manager -y setopt fedora-nvidia-580.enabled=0
 
 dnf autoremove -y
