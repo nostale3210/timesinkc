bc16eb37b6a9c6f465437c3de9b5af4773e67ab7
fei <fei@timesink>
2025-12-10 15:16:27 +0100

    include nvidia-container-toolkit

--- scripts/nvidia.sh
+++ scripts/nvidia.sh
@@ -8,10 +8,14 @@ install -Dm644 /tmp/certs/private_key.priv /etc/pki/akmods/private/private_key.p
 
 dnf copr enable -y bieszczaders/kernel-cachyos-lto
 dnf config-manager -y addrepo --from-repofile=https://negativo17.org/repos/fedora-nvidia-580.repo
+dnf config-manager -y addrepo --from-repofile=https://nvidia.github.io/libnvidia-container/stable/rpm/nvidia-container-toolkit.repo
 
 dnf install -y akmods --from-repo copr:copr.fedorainfracloud.org:bieszczaders:kernel-cachyos-lto
 
 dnf install -y nvidia-driver akmod-nvidia libva-nvidia-driver nvidia-driver-cuda
+
+dnf install -y nvidia-container-toolkit nvidia-container-toolkit-base \
+    libnvidia-container-tools libnvidia-container1
 
 # KVER="$(rpm -q kernel-core --queryformat '%{VERSION}-%{RELEASE}')"
 KVER_LONG="$(rpm -q kernel-cachyos-lto --queryformat '%{VERSION}-%{RELEASE}.%{ARCH}')"
@@ -29,6 +33,6 @@ modinfo -l /usr/lib/modules/"${KVER_LONG}"/extra/nvidia/nvidia.ko.xz
 rm -rf /etc/pki/akmods/private/private_key.priv
 
 dnf copr disable -y bieszczaders/kernel-cachyos-lto
-dnf config-manager -y setopt fedora-nvidia-580.enabled=0
+dnf config-manager -y setopt fedora-nvidia-580.enabled=0 nvidia-container-toolkit.enabled=0
 
 dnf autoremove -y
