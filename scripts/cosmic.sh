b6f9180bd8ff4b79f33b7a4448d468ff5c399366
fei <fei@timesink>
2025-12-20 21:26:42 +0100

    Revert "nvidia from terra repo"
    This reverts commit a84f6261f8ac18f4e079047edf5fcb544f477977.

--- scripts/cosmic.sh
+++ scripts/cosmic.sh
@@ -1,12 +1,17 @@
 #!/usr/bin/env bash
 set -oue pipefail
 
+dnf config-manager -y setopt rpmfusion-free.enabled=1 rpmfusion-free-updates.enabled=1 \
+    rpmfusion-nonfree.enabled=1 rpmfusion-nonfree-updates.enabled=1
+
 dnf install -y @cosmic-desktop-environment --skip-broken \
     --exclude=thunderbird,gnome-calculator,nheko,okular,rhythmbox,abrt,libreoffice-core \
     --exclude=firefox,setroubleshoot,librewolf,toolbox
 dnf install -y wiremix gnome-tweaks
 
-dnf config-manager -y setopt terra.enabled=0 terra-mesa.enabled=0 terra-multimedia.enabled=0
+dnf config-manager -y setopt rpmfusion-free.enabled=0 rpmfusion-free-updates.enabled=0 \
+    rpmfusion-nonfree.enabled=0 rpmfusion-nonfree-updates.enabled=0 \
+    terra.enabled=0 terra-mesa.enabled=0 terra-multimedia.enabled=0
 
 rm -rf /etc/systemd/system/display-manager.service
 systemctl enable cosmic-greeter.service
--- scripts/nvidia.sh
+++ scripts/nvidia.sh
@@ -7,20 +7,19 @@ mkdir -p /var/lib/alternatives
 install -Dm644 /tmp/certs/private_key.priv /etc/pki/akmods/private/private_key.priv
 
 dnf copr enable -y bieszczaders/kernel-cachyos-lto
-dnf config-manager -y setopt terra.enabled=1
+dnf config-manager -y addrepo --from-repofile=https://negativo17.org/repos/fedora-nvidia-580.repo
 dnf config-manager -y addrepo --from-repofile=https://nvidia.github.io/libnvidia-container/stable/rpm/nvidia-container-toolkit.repo
-dnf install -y terra-release-nvidia
 
 dnf install -y akmods --from-repo copr:copr.fedorainfracloud.org:bieszczaders:kernel-cachyos-lto
 
-dnf install -y nvidia-driver-580 libva-nvidia-driver nvidia-driver-580-cuda
+dnf install -y nvidia-driver akmod-nvidia libva-nvidia-driver nvidia-driver-cuda
 
 dnf install -y nvidia-container-toolkit nvidia-container-toolkit-base \
     libnvidia-container-tools libnvidia-container1
 
 # KVER="$(rpm -q kernel-core --queryformat '%{VERSION}-%{RELEASE}')"
 KVER_LONG="$(rpm -q kernel-cachyos-lto --queryformat '%{VERSION}-%{RELEASE}.%{ARCH}')"
-NVIDIA_AKMOD_VERSION="$(rpm -q "akmod-nvidia-580" --queryformat '%{VERSION}-%{RELEASE}')"
+NVIDIA_AKMOD_VERSION="$(rpm -q "akmod-nvidia" --queryformat '%{VERSION}-%{RELEASE}')"
 
 akmods --force \
     --kernels "${KVER_LONG}" \
@@ -34,4 +33,4 @@ modinfo -l /usr/lib/modules/"${KVER_LONG}"/extra/nvidia/nvidia.ko.xz
 rm -rf /etc/pki/akmods/private/private_key.priv
 
 dnf copr disable -y bieszczaders/kernel-cachyos-lto
-dnf config-manager -y setopt terra.enabled=0 terra-nvidia.enabled=0 nvidia-container-toolkit.enabled=0
+dnf config-manager -y setopt fedora-nvidia-580.enabled=0 nvidia-container-toolkit.enabled=0
