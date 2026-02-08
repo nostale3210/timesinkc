2bd56ae777b857970089da216ab06e1b7409c8ec
fei <fei@timesink>
2026-02-08 16:09:48 +0100

    Revert "build wlroots, mango wl-only branch from source"
    This reverts commit 69faeddbd5d086b70ed7e8ac83ab45819988bb40.

--- scripts/niri/mango-git.sh
+++ scripts/niri/mango-git.sh
@@ -1,29 +1,17 @@
 #!/usr/bin/env bash
 set -oue pipefail
 
-dnf install -y xdg-desktop-portal-wlr xdg-desktop-portal-gtk \
-    gcc gcc-c++ cmake glibc wayland-devel wayland-protocols-devel \
-    libinput-devel libdrm-devel libxkbcommon-devel \
-    pixman-devel meson ninja-build libdisplay-info-devel \
-    libliftoff-devel hwdata-devel libseat-devel \
-    pcre2-devel xorg-x11-server-Xwayland xorg-x11-server-Xwayland-devel \
-    libxcb-devel xcb-util-wm-devel mesa-libgbm-devel glslang-devel \
-    vulkan-loader-devel libwayland-egl libglvnd-egl libglvnd-gles \
-    lcms2-devel xcb-util-renderutil-devel xcb-util-errors-devel \
-    cairo-devel libglvnd-devel
-
-git clone https://gitlab.freedesktop.org/wlroots/wlroots.git
-pushd wlroots
-meson setup build/
-ninja -C build/ install
-mv /usr/local/lib64/libwlroots-* /usr/lib64/
-popd
-rm -rf wlroots
+dnf install -y xdg-desktop-portal-wlr xdg-desktop-portal-gtk wlroots-devel scenefx-devel
+dnf install -y gcc gcc-c++ cmake glibc wayland-devel wayland-protocols-devel
+dnf install -y libinput-devel libdrm-devel libxkbcommon-devel
+dnf install -y pixman-devel meson ninja-build libdisplay-info-devel
+dnf install -y libliftoff-devel hwdata-devel libseat-devel
+dnf install -y pcre2-devel xorg-x11-server-Xwayland xorg-x11-server-Xwayland-devel
+dnf install -y libxcb-devel xcb-util-wm-devel
 
 git clone https://github.com/DreamMaoMao/mangowc.git
 pushd mangowc
-git switch wl-only
-meson setup build -Dprefix=/usr
+meson build -Dprefix=/usr
 ninja -C build install
 popd
 rm -rf mangowc
